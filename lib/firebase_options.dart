fef519db744597a3c9c01f7012f5587de6e0e37a
laya666-design <laya666@gmail.com>
2026-08-16 00:20:59 +0100

    Update Firebase options for Android
    Updated Firebase options for Android platform with new credentials.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,26 +1,29 @@
-// File generated from your google-services.json
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError('Web not configured - use Android');
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web.',
+      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
       default:
-        return android;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
     }
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyB7Uo11KWLPdppyXdr0oFcv8002ldmLsq8',
-    appId: '1:1030419687237:android:7b3f91900687e2bfa3002c',
-    messagingSenderId: '1030419687237',
-    projectId: 'ajalaks-el-bouni',
-    storageBucket: 'ajalaks-el-bouni.firebasestorage.app',
+    apiKey: 'AIzaSyBHpypQOAQnMmifFtJcX97Mnbrb8ORnd6U',
+    appId: '1:994131871524:android:25fd653f1e6da976ea15b1',
+    messagingSenderId: '994131871524',
+    projectId: 'fakerni-b96c2',
+    storageBucket: 'fakerni-b96c2.firebasestorage.app',
   );
 }
