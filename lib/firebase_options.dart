1c664f5192321f88adbeaf18b3e2ecd503bb39ea
laya666-design <laya666@gmail.com>
2026-08-16 12:09:09 +0100

    Refactor DefaultFirebaseOptions for better clarity

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,28 +1,28 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-classe DefaultFirebaseOptions {
-  statique FirebaseOptions obtenir currentPlatform {
-    si (kIsWeb) {
-      lancer Une erreur non prise en charge(
-        Les options DefaultFirebaseOptions n'ont pas été configurées pour le Web.
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web.',
       );
     }
     switch (defaultTargetPlatform) {
-      cas TargetPlatform.android :
-        retourner Android ;
-      défaut:
-        lancer Une erreur non prise en charge(
-          Les options DefaultFirebaseOptions ne sont pas prises en charge par cette plateforme.
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
         );
     }
   }
 
-  const statique FirebaseOptions android = FirebaseOptions(
-    Clé API : 'AIzaSyBHpypQOAQnMmifFtJcX97Mnbrb8ORnd6U',
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyBHpypQOAQnMmifFtJcX97Mnbrb8ORnd6U',
     appId: '1:994131871524:android:25fd653f1e6da976ea15b1',
     messagingSenderId: '994131871524',
-    ID du projet : « fakerni-b96c2 »,
-    storageBucket : 'fakerni-b96c2.firebasestorage.app',
+    projectId: 'fakerni-b96c2',
+    storageBucket: 'fakerni-b96c2.firebasestorage.app',
   );
 }
